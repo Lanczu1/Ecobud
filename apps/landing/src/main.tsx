58e99e05730ce87ffaa2d98f211e7b9ac7de5580
Lanczu1 <streamlancetv@gmail.com>
2026-09-14 15:36:04 +0800

    Landing Page Update

--- apps/landing/src/main.tsx
+++ apps/landing/src/main.tsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react';
+import { StrictMode, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import logoUrl from '../../logo/logo.png';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+function Root() {
+  useEffect(() => {
+    let link: HTMLLinkElement | null = document.querySelector("link[rel~='icon']");
+    if (!link) {
+      link = document.createElement('link');
+      link.rel = 'icon';
+      document.head.appendChild(link);
+    }
+    link.type = 'image/png';
+    link.href = logoUrl;
+  }, []);
+
+  return (
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+}
+
+createRoot(document.getElementById('root')!).render(<Root />);
